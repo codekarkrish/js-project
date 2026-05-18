fdfe9f19d508fb70d0d92df0fe7f0a5987e8a8a7
codekarkrish <chykrish298@gmail.com>
2026-05-19 00:08:19 +0530

    added download QR feature

--- QRGenerater/qr.js
+++ QRGenerater/qr.js
@@ -1,25 +1,49 @@
 // Script.js
+
 // create a new QRCode instance
 let qrcode = new QRCode(
     document.querySelector(".qrcode")
 );
+
 // Initial QR code generation
-// with a default message
 qrcode.makeCode("Why did you scan me?");
-// Function to generate QR
-// code based on user input
+
+// Function to generate QR code based on user input
 function generateQr() {
-    if (
-        document.querySelector("input")
-            .value === "" ||
-        document.querySelector("input")
-            .value === " ") {
-        alert(
-            "Input Field Can not be blank!"
-        );} 
+    let input = document.querySelector("input").value;
+
+    if (input === "" || input === " ") {
+        alert("Input Field Can not be blank!");
+    } else {
+        qrcode.makeCode(input);
+    }
+}
+
+// Function to download QR code
+function downloadQR() {
+    let qrContainer = document.querySelector(".qrcode");
+
+    // QRCode.js usually creates a canvas
+    let canvas = qrContainer.querySelector("canvas");
+
+    if (canvas) {
+        let link = document.createElement("a");
+        link.href = canvas.toDataURL("image/png");
+        link.download = "qrcode.png";
+        link.click();
+    } 
     else {
-        qrcode.makeCode(
-            document.querySelector(
-                "input"
-            ).value);
-}}
+        // fallback for image output
+        let img = qrContainer.querySelector("img");
+
+        if (img) {
+            let link = document.createElement("a");
+            link.href = img.src;
+            link.download = "qrcode.png";
+            link.click();
+        } 
+        else {
+            alert("QR code not generated yet!");
+        }
+    }
+}
